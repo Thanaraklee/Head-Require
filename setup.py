0a177b5ce07cb46fe88a09191396c4a342b7fa4d
Thanarak Leenanon <bzank.lee@hotmail.com>
2024-03-31 03:30:53 +0700

    Upload config

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 
 setup(
     name='head_require',
-    version='0.1.0',
+    version='0.1.2',
     description='head-require is a library that aims to simplify the creation of requirements.txt files. head-require generates requirements.txt based on the packages actually used in your project.',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
